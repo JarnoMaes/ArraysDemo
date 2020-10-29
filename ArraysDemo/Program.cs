a870d00970ece4da1d87460fd34663b7d30e1b90
JARNO MAES <JARNO.MAES@student.intecbrussel.be>
2020-10-29 16:20:30 +0100

    Add project files.

---
+++ ArraysDemo/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] movies = { "LOTR", "Fight Club", "Interstellar", "Fight Night" };
+            //for (int i = 0; i < movies.Length; i++)
+            //{
+            //    int rank = i + 1;
+            //    Console.WriteLine(rank + ". " + movies[i] );
+            //}
+
+            //Console.ReadLine();
+
+
+            string[] movies = new string[4];
+
+
+            Console.WriteLine("Type in 4 movies");
+            
+            for (int i = 0; i < movies.Length; i++)
+            {
+                movies[i] = Console.ReadLine();
+            }
+           
+            
+            Console.WriteLine("Here they are Alpha:  ");
+            Array.Sort(movies);
+
+            for (int i = 0; i < movies.Length; i++)
+            {
+                Console.WriteLine(movies[i]);
+            }
+            
+            Console.ReadLine();
+            
+           
+            
+            
+            
+            
+
+
+            
+
+
+
+
+        }
+    }
+}
